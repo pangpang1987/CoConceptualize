0e485d5672ff49973ecb36e015d60dcf74d1323f
pangpang1987 <pangpang.i.lily@gmail.com>
2012-08-22 19:00:28 +0800

    register form - static version

---
+++ js/register.js
@@ -0,0 +1,102 @@
+$(function(){
+	$("button").button();
+})
+
+
+$(function(){
+	$("input").focus(function(){
+		$(this).parent().children(".info").css("display","block");
+		$(this).parent().children(".info").css("color","white");
+		$(this).parent().children(".empty").css("display","none");	
+	})
+	
+	$("input").blur(function(){
+		if(!$(this).val()){
+			$(this).parent().children(".empty").css("display","block");	
+		}
+		$(this).parent().children(".info").css("display","none");
+	})
+})
+
+$(function(){
+	
+	$("#name").blur(function(){
+		if(!isName($(this).val())){
+			$("#error_name").css("display","block");
+		}
+		else{
+			$("#error_name").css("display","none");
+		}
+		
+		if(!isLong($(this).val())){
+			$("#info_name").css("display","block");
+			$("#info_name").css("color","#FE57A1");
+		}
+	})
+	
+	$("#email").blur(function(){
+		if(!isEmail($("#email").val())){
+			$("#error_email").css("display","block");
+		}
+		else{
+			$("#error_email").css("display","none");
+		}
+			
+		
+	})	
+	
+	$("#password").blur(function(){
+		if(!isPassword($(this).val())){
+			$("#error_pwd").css("display","block");
+		}
+		else{
+			$("#error_pwd").css("display","none");
+		}
+		if(!isLong($(this).val())){
+			$("#info_pwd").css("display","block");
+			$("#info_pwd").css("color","#FE57A1");
+		}
+	})
+	
+	$("#password_confirm").blur(function(){
+		if($(this).val() != $("#password").val()){
+			$("#error_pwd_c").css("display","block");
+		}
+		else{$(this).css("background-color","white")
+			$("#error_pwd_c").css("display","none");
+		}
+	})
+})
+
+
+
+function isEmail(strEmail) {
+	if (strEmail.search(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/) != -1)
+	
+	return true;
+	else
+	return false;
+}
+function isName(strName){
+	if (strName.search(/^[A-z][A-z0-9]*$/g) != -1)
+	return true;
+	else
+	return false;
+}
+
+function isPassword(str){
+	if(str != $("#name").val() || str.length == 0)
+	return true;
+	else
+	return false;
+}
+
+var minlength = 5;
+
+function isLong(str){
+	if(str.length == 0)
+	return true;
+	if(str.length < minlength || str.length > 50)
+	return false;
+	return true;
+}
